70aad959925791371900588c99ade308a59031c4
Eliseu Ferreira dos Santos <Eliseu.Santos@gs1br.org>
2024-02-26 13:27:57 -0300

    Projeto iniciado

---
+++ Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/LojaTenisApplication.java
@@ -0,0 +1,13 @@
+package br.senac.tads.pi.lojatenis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaTenisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaTenisApplication.class, args);
+	}
+
+}
