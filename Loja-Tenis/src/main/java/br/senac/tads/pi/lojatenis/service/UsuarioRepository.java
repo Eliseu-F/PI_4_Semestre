59ea4c8eedc0d42228ad7462c79f982608971f52
Letícia <iambragalets@gmail.com>
2024-03-01 22:17:09 -0300

    ADICIONADO A SESSÃO DE LOGIN QUE VERIFICA SE O EMAIL E A SENHA SAO OS MESMOS CADASTRADOS NO BANCO DE DADOS, ENVIADO PELO FORMULARIO DO SIGNUP.HTML E VERIFICADO POR MEIO DO SIGNUPCONTROLLER

--- Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/service/UsuarioRepository.java
+++ Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/service/UsuarioRepository.java
@@ -3,6 +3,10 @@ package br.senac.tads.pi.lojatenis.service;
 import br.senac.tads.pi.lojatenis.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    
+
+    Optional<Usuario> findByEmail(String email);
+
 }
