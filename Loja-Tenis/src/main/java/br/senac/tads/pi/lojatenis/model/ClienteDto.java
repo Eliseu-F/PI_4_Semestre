d3d76ea9217300b829fa1ad9d6d1df1f212407b0
Eliseu Ferreira dos Santos <Eliseu.Santos@gs1br.org>
2024-04-08 16:48:18 -0300

    Inserindo lógica do security para testes

---
+++ Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/model/ClienteDto.java
@@ -0,0 +1,26 @@
+package br.senac.tads.pi.lojatenis.model;
+
+/**
+ *
+ * @author eliseu.santos
+ */
+public class ClienteDto {
+    
+    private int id;
+
+    private String nome;
+    private int dataNascimento;
+    private String genero;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    //Endereço
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+}
