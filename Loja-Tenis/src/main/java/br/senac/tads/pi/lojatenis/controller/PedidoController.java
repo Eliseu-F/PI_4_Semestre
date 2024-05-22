856d156cbf6360bb1ac8e253bc19eae55264b18a
Eliseu Ferreira dos Santos <Eliseu.Santos@gs1br.org>
2024-05-22 15:22:21 -0300

    Criada tela de visualizar pedidos - inserir localhost:8080/pedido/MostraPedidos

---
+++ Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/controller/PedidoController.java
@@ -0,0 +1,37 @@
+package br.senac.tads.pi.lojatenis.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.senac.tads.pi.lojatenis.model.Pedido;
+import br.senac.tads.pi.lojatenis.service.PedidoRepository;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/pedido")
+@Service
+public class PedidoController {
+    @Autowired
+    private PedidoRepository repo;
+
+    @GetMapping({"/MostraPedidos"})
+    public String showPedidosList(Model model, HttpSession session) {
+        List<Pedido> pedidos = repo.findAll(Sort.by(Sort.Direction.DESC, "dataPedido"));
+        model.addAttribute("pedidos", pedidos);
+
+        return "pedidos/MostraPedidos";
+    }
+
+    @GetMapping("/pedidos")
+    public String getPedido() {
+        return "pedidos/MostraPedidos";
+    }
+
+}
