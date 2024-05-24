df869b2063d1e584e00e6299982e87595e556c96
Alexsandro Alves <Alex@adm-info.com>
2024-05-23 23:43:48 -0300

    Adicionado as funcionalidades para alterar o status dos pedidos

--- Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/controller/PedidoController.java
+++ Loja-Tenis/src/main/java/br/senac/tads/pi/lojatenis/controller/PedidoController.java
@@ -1,46 +1,51 @@
 package br.senac.tads.pi.lojatenis.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import br.senac.tads.pi.lojatenis.model.ItemPedido;
+import br.senac.tads.pi.lojatenis.model.StatusUpdateDto;
+
 import br.senac.tads.pi.lojatenis.model.Pedido;
-import br.senac.tads.pi.lojatenis.service.PedidoRepository;
+import br.senac.tads.pi.lojatenis.service.AtualizarProdutoRepository;
 import jakarta.servlet.http.HttpSession;
+import java.util.List;
+import org.springframework.data.domain.Sort;
 
 @Controller
 @RequestMapping("/pedido")
-@Service
 public class PedidoController {
     @Autowired
-    private PedidoRepository repo;
+    private AtualizarProdutoRepository repo;
 
-    @GetMapping({"/MostraPedidos"})
+    @GetMapping("/MostraPedidos")
     public String showPedidosList(Model model, HttpSession session) {
         List<Pedido> pedidos = repo.findAll(Sort.by(Sort.Direction.DESC, "dataPedido"));
         model.addAttribute("pedidos", pedidos);
-        List<List<ItemPedido>> detalhesProdutos = new ArrayList<>();
-        for (Pedido pedido : pedidos) {
-            List<ItemPedido> itensPedido = pedido.getItens();
-            detalhesProdutos.add(itensPedido);
+        return "pedidos/MostraPedidos";
+    }
+
+    @PutMapping("/updateStatus/{id}")
+    @ResponseBody
+    public ResponseEntity<?> updateStatus(@PathVariable Long id, @RequestBody StatusUpdateDto statusUpdate) {
+        Optional<Pedido> pedidoOpt = repo.findById(id);
+        if (pedidoOpt.isPresent()) {
+            Pedido pedido = pedidoOpt.get();
+            String newStatus = statusUpdate.getStatus();
+            pedido.setStatus(newStatus);
+            repo.save(pedido);
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
         }
-        model.addAttribute("detalhesProdutos", detalhesProdutos);
-
-        return "pedidos/MostraPedidos";
     }
-
-    @GetMapping("/pedidos")
-    public String getPedido() {
-        return "pedidos/MostraPedidos";
-    }
-    
-
 }
